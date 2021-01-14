57414b4d9aa155b34144779771b33cc2a9e21a81
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-01-14 14:22:30 -0600

    Create logic

--- js/site.js
+++ js/site.js
@@ -1,44 +1,74 @@
-const fearsForm = document.querySelector('#fears-form');
+const fearsForm = document.querySelector('#sunset-form');
 const numInputs = document.querySelectorAll('.numInput');
-const sum = document.querySelector('#sum');
 const output = document.querySelector('#output');
+const generateNumbers = document.querySelector('#generate-numbers')
 
-fearsForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  printSolution();
-})
+
+
 
 // parse inputs to num and return results as array
 const parseInputToNum = () => {
   return [...numInputs].map(index => parseInt(index.value));
-}
+};
 
 const findSolution = () => {
   const numberArray = parseInputToNum();
   console.log(numberArray);
   const solutionArray = [];
+  let bigNum = numberArray[0];
   
-    numberArray.forEach(num1 => {
-      numberArray.forEach(num2 => {
+  // Push the first number as it will
+  // always see the sunset
+  solutionArray.push(numberArray[0]);
 
-        // Check sum
-        if (num1 + num2 === parseInt(sum.value)) {
-          solutionArray.push(`${num1} + ${num2} = ${sum.value}`);
-        }
-      })
-    })
 
-  return solutionArray.length > 0 ? solutionArray : 'No matching sum could be found.';
+  for (let index = 0; index < numberArray.length; index++) {
+    if (numberArray[index] > bigNum) {
+      solutionArray.push(numberArray[index]);
+      bigNum = numberArray[index];
+    }
+  }
+
+    // numberArray.forEach(num1 => {
+    //   numberArray.forEach(num2 => {
+
+    //     // Check sum
+    //     if (num1 + num2 === parseInt(sum.value)) {
+    //       solutionArray.push(`${num1} + ${num2} = ${sum.value}`);
+    //     }
+    //   })
+    // })
+
+  return solutionArray;
+};
+
+const addComas = () => {
+
+};
+
+const populateRandomNumbers = () => {
+  for (let index = 0; index < numInputs.length; index++) {
+    let randomNumber = Math.floor(Math.random() * 9 + 1);
+    numInputs[index].value = randomNumber;
+    
+  }
 };
 
 const printSolution = () => {
   output.innerHTML = '';
   const solution = findSolution();
-  if (Array.isArray(solution)) {
+  let outPutHTML = '<p>[';
     solution.forEach(result => {
-      output.innerHTML += `<p>${result}</p>`;
+      output.innerHTML += `${result}, `;
     })
-  } else {
-    output.innerHTML = `<p>${solution}</p>`;
-  }
-}
+    outPutHTML += ']</p>'
+};
+
+window.onload = populateRandomNumbers();
+
+generateNumbers.addEventListener('click', populateRandomNumbers);
+
+fearsForm.addEventListener('click', (event) => {
+  event.preventDefault();
+  printSolution();
+})
